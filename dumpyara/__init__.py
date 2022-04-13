ca30a8aaa02ea7876b20e9dfd61df7ca2c62c33e
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-04-13 15:26:26 +0200

    dumpyara: Use Path.cwd()
    Change-Id: Ib926c140bc77e44d3cd62d8378bfd569cbc6b2a6

--- dumpyara/__init__.py
+++ dumpyara/__init__.py
@@ -4,10 +4,9 @@
 # SPDX-License-Identifier: GPL-3.0
 #
 
-import os
 from pathlib import Path
 
 __version__ = "1.0.0"
 
 module_path = Path(__file__).parent
-current_path = Path(os.getcwd())
+current_path = Path.cwd()
