fdfea3cb106018a05431d5c7927420f3614de65e
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-04-25 00:35:09 +0200

    dumpyara: Make Codacy happy
    Change-Id: If58834bbd0c19de63afcccada10fea10ffa716b5

--- dumpyara/__init__.py
+++ dumpyara/__init__.py
@@ -3,6 +3,7 @@
 #
 # SPDX-License-Identifier: GPL-3.0
 #
+"""Dumpyara library."""
 
 from pathlib import Path
 
--- dumpyara/lib/libsevenz/__init__.py
+++ dumpyara/lib/libsevenz/__init__.py
@@ -3,6 +3,7 @@
 #
 # SPDX-License-Identifier: GPL-3.0
 #
+"""7z wrapper."""
 
 from subprocess import check_output
 
