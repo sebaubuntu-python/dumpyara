fbc9dc26d5b5f9ccee8578e85c4c0f679fbd6b7d
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-04-25 00:23:41 +0200

    dumpyara: utils: bootimg: Fix extraction with sebaubuntu_libs AIKManager
    Change-Id: I5255dc80a178d9dc7ceee42d003f4c85bad585ea

--- dumpyara/utils/bootimg.py
+++ dumpyara/utils/bootimg.py
@@ -12,7 +12,7 @@ from sebaubuntu_libs.liblogging import LOGW
 def extract_bootimg(file: Path, output_path: Path):
 	aik_manager = AIKManager()
 	try:
-		image_info = aik_manager.extract(file)
+		image_info = aik_manager.unpackimg(file)
 	except Exception:
 		LOGW(f"Failed to extract {file.name}, invalid boot image")
 		return None
@@ -24,14 +24,14 @@ def extract_bootimg(file: Path, output_path: Path):
 	if image_info.kernel:
 		(output_path / "kernel").write_bytes(image_info.kernel.read_bytes())
 
-	if image_info.dt_image:
-		(output_path / "dt.img").write_bytes(image_info.dt_image.read_bytes())
+	if image_info.dt:
+		(output_path / "dt.img").write_bytes(image_info.dt.read_bytes())
 
-	if image_info.dtb_image:
-		(output_path / "dtb.img").write_bytes(image_info.dtb_image.read_bytes())
+	if image_info.dtb:
+		(output_path / "dtb.img").write_bytes(image_info.dtb.read_bytes())
 
-	if image_info.dtbo_image:
-		(output_path / "dtbo.img").write_bytes(image_info.dtbo_image.read_bytes())
+	if image_info.dtbo:
+		(output_path / "dtbo.img").write_bytes(image_info.dtbo.read_bytes())
 
 	if image_info.ramdisk:
 		copy_tree(str(image_info.ramdisk), str(output_path / "ramdisk"), preserve_symlinks=True)
